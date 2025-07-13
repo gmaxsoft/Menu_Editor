38ce3bc60242b8e84b4043173678adb3220c8f8b
Grzegorz Miśków <133238725+gmaxsoft@users.noreply.github.com>
2025-07-14 01:50:48 +0200

    Final version

--- vite.config_dist.ts
+++ vite.config_dist.ts
@@ -1,31 +1,27 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
-import dts from "vite-plugin-dts";
+// Usunięto import dts from "vite-plugin-dts";
 
 export default defineConfig({
   build: {
-    outDir: 'lib',
+    outDir: 'dist',
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
       name: "MenuEditor",
-      fileName(format, entryName) {
-        let strFormat = '';
-        if (format != 'es' ) {
-          strFormat = `.${format}`;
-        }
-        return `${entryName}${strFormat}.js`;
+      formats: ['umd'],
+      fileName: (format) => {
+        return `menu-editor.js`;
       },
     },
     rollupOptions: {
       output: {
         assetFileNames: (assetInfo) => {
-          if (assetInfo.name == 'style.css') {
-            return 'css/styles.css';
+          if (assetInfo.name === 'style.css') {
+            return 'styles.min.css';
           }
           return `[name][hash][extname]`;
         }
       }
     }
   },
-  plugins: [dts()],
 });
